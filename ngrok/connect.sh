e759702ecc58aac382e1ce914de3cdd27e26623d
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-14 08:20:44 +0100

    sogivox442@maazios.com

--- ngrok/connect.sh
+++ ngrok/connect.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-echo "Replace <TOKEN> with the one obtained in: https://dashboard.ngrok.com/..."
-read key
-docker run -it --rm --name ngrok --network=testnet -e NGROK_AUTHTOKEN=<TOKEN> ngrok/ngrok:latest http http://ethereum-geth:7396
+docker run -it --rm --name ngrok --network=testnet -e NGROK_AUTHTOKEN=2Mzg7mT5Xdbb4VCQ3VvtpE4kNrA_6W8eMDEddVRVEqZtrhF9i ngrok/ngrok:latest http http://ethereum-geth:7396
--- ngrok/run.sh
+++ ngrok/run.sh
@@ -2,4 +2,4 @@
 
 echo "Replace <TOKEN> with the one obtained in: https://dashboard.ngrok.com/..."
 read key
-docker run -it --rm --name=ngrok --network=testnet -v $(pwd)/ngrok.yml:/etc/ngrok.yml -e NGROK_CONFIG=/etc/ngrok.yml -e NGROK_AUTHTOKEN=<TOKEN> ngrok/ngrok:latest start --all
+docker run -it --rm --name=ngrok --network=testnet -v $(pwd)/ngrok.yml:/etc/ngrok.yml -e NGROK_CONFIG=/etc/ngrok.yml ngrok/ngrok:latest start --all
