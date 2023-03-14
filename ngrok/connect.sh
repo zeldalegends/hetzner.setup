e91b98cbbc703a714913bea0df89c840e1a6915e
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-14 07:19:48 +0100

    replace <TOKEN>

--- ngrok/connect.sh
+++ ngrok/connect.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./ngrok http http://localhost:7396
+echo "Replace <TOKEN> with the one obtained in: https://dashboard.ngrok.com/..."
+read key
+docker run --rm --name ngrok --net=fiware_default -it -v $(pwd)/ngrok.yml:/etc/ngrok.yml -e NGROK_CONFIG=/etc/ngrok.yml -e NGROK_AUTHTOKEN=<TOKEN> ngrok/ngrok:latest http http://localhost:7396
