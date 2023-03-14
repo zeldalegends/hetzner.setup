4f3f345ff91d59dd43e03ed456e6af0cf2819109
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-14 07:17:30 +0100

    replace <TOKEN>

---
+++ ngrok/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Replace <TOKEN> with the one obtained in: https://dashboard.ngrok.com/..."
+read key
+docker run --rm --name ngrok --net=fiware_default -it -v $(pwd)/ngrok.yml:/etc/ngrok.yml -e NGROK_CONFIG=/etc/ngrok.yml -e NGROK_AUTHTOKEN=<TOKEN> ngrok/ngrok:latest start --all
