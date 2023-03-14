d9e7a159e5d732cacca01ae5695036bbc7798bd8
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-14 08:22:12 +0100

    Update connect.sh

--- ngrok/connect.sh
+++ ngrok/connect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --name ngrok --network=testnet -e NGROK_AUTHTOKEN=2Mzg7mT5Xdbb4VCQ3VvtpE4kNrA_6W8eMDEddVRVEqZtrhF9i ngrok/ngrok:latest http http://ethereum-geth:7396
+docker run -it --rm --name ngrok --network=testnet -e NGROK_AUTHTOKEN=2Mzg7mT5Xdbb4VCQ3VvtpE4kNrA_6W8eMDEddVRVEqZtrhF9i ngrok/ngrok:latest http ethereum-geth:7396
