68873622a23bfa7b87fee5c554d9a05c0a5b4a80
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-14 08:01:12 +0100

    Update run.sh

--- geth/run.sh
+++ geth/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -d --name ethereum-geth -e PUID=$(id -u) -e PGID=$(id -g) -e TZ=Europe/Amsterdam -v $(pwd)/config:/config --restart unless-stopped ethereum-geth
+docker build . -t ethereum-geth
+docker network create testnet
+docker run -d --name=ethereum-geth --network=testnet -e PUID=$(id -u) -e PGID=$(id -g) -e TZ=Europe/Amsterdam -v $(pwd)/config:/config --restart=unless-stopped ethereum-geth
