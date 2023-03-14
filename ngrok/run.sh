f10c82bcfba9c2222b0bbeb0a1f011e1740aef45
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-14 08:22:43 +0100

    Update run.sh

--- ngrok/run.sh
+++ ngrok/run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-echo "Replace <TOKEN> with the one obtained in: https://dashboard.ngrok.com/..."
-read key
 docker run -it --rm --name=ngrok --network=testnet -v $(pwd)/ngrok.yml:/etc/ngrok.yml -e NGROK_CONFIG=/etc/ngrok.yml ngrok/ngrok:latest start --all
