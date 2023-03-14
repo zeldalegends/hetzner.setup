12a9c75ef344aa00b25bb6d43fe3fcab8eae52e4
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-14 05:31:43 +0100

    Create docker.sh

---
+++ ngrok/docker.sh
@@ -0,0 +1,3 @@
+echo Replace <TOKEN> with the one obtained in here: https://dashboard.ngrok.com/
+read key
+docker run --rm --name ngrok --net=fiware_default -it -v $(pwd)/ngrok.yml:/etc/ngrok.yml -e NGROK_CONFIG=/etc/ngrok.yml -e NGROK_AUTHTOKEN=<TOKEN> ngrok/ngrok:latest start --all
