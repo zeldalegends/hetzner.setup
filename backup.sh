8af15a5febf0d4752e33244ff8c2324c9843161d
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-15 04:35:57 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-echo Copy this file to the root of the folders to backup...
+
+echo Backup folder: $(pwd)
+echo Press Enter to start...
 read key
-tar czvf backup_$(date +%Y%m%d%H%M%S).tar.gz .
+tar czvf $(basename $(pwd))_$(date +%Y%m%d%H%M%S).tar.gz .
+mkdir public-tmp
+mv $(basename $(pwd))*.tar.gz public-tmp/
+docker run -d --rm -p 2010:80 --name apache-tmp -v ./public-tmp:/usr/local/apache2/htdocs/ httpd:alpine
+echo  -e "\033[1;33mDownload file at: http://$(ip addr show $(ip route | awk '/default/ { print $5 }') | grep "inet" | head -n 1 | awk '/inet/ {print $2}' | cut -d'/' -f1):2010\033[0m"
+echo Press Enter to stop the Apache server, CTRL+C to terminate...
+readk key
+docker stop apache-tmp
+docker rm apache-tmp
+echo Press Enter to delete public folder and backup files, CTRL+C to terminate...
+readk key
+sudo rm -r public-tmp
