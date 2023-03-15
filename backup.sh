d97db7107168c493739fa8547d054cdeb7bbc593
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-15 05:00:53 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-echo Backup folder: $(pwd)
-echo Press Enter to start...
-read key
+echo -e "\033[1;36mBackup folder: $(pwd)\033[0m"
+read -p "Do you want to proceed? (y/n) " yn
+case $yn in
+        [yY] ) echo Ok, backup started.;
+                break;;
+        [nN] ) echo Exiting...;
+                exit;;
+        * ) echo Default behaviour. Backup started.;;
+esac
 tar czvf $(basename $(pwd))_$(date +%Y%m%d%H%M%S).tar.gz .
 mkdir public-tmp
 mv $(basename $(pwd))*.tar.gz public-tmp/
 docker run -d --rm -p 2010:80 --name apache-tmp -v ./public-tmp:/usr/local/apache2/htdocs/ httpd:alpine
 echo  -e "\033[1;33mDownload file at: http://$(ip addr show $(ip route | awk '/default/ { print $5 }') | grep "inet" | head -n 1 | awk '/inet/ {print $2}' | cut -d'/' -f1):2010\033[0m"
-echo Press Enter to stop the Apache server, CTRL+C to terminate...
-readk key
+echo Press Enter to stop the Apache server and delete files...
+read key
 docker stop apache-tmp
-docker rm apache-tmp
-echo Press Enter to delete public folder and backup files, CTRL+C to terminate...
-readk key
+#docker rm apache-tmp
 sudo rm -r public-tmp
