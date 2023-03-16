d734673aac19c29a24dffddc181022e3dcfc757e
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-16 16:52:44 +0100

    Update template-ssh-at-client-side.bat

--- template-ssh-at-client-side.bat
+++ template-ssh-at-client-side.bat
@@ -1,20 +1,20 @@
 @echo off
-rem Template to configure SSH access from the client machine
+rem # Template to configure SSH access from the client machine
 
 set serverip=...
 
-rem Access the server and change the root password
+rem # Access the server and change the root password
 ssh root@%serverip% -o ServerAliveInterval=100
 
-rem Create a key pair for SSH authentication (RSA)
-rem set filepath=$env:USERPROFILE\.ssh\id_rsa.pub 
-rem ssh-keygen [-f %filepath%]
+rem # Create a key pair for SSH authentication (RSA)
+rem # set filepath=$env:USERPROFILE\.ssh\id_rsa.pub 
+rem # ssh-keygen [-f %filepath%]
 ssh-keygen
 
-rem Copy the public key (to the server)
-rem Under WSL: ssh-copy-id [-i %filepath%] root@<serverip>
-rem type %filepath%.pub | ssh root@%serverip% "cat >> .ssh/authorized_keys"
+rem # Copy the public key (to the server)
+rem # Under WSL: ssh-copy-id [-i %filepath%] root@%serverip%
+rem # type %filepath%.pub | ssh root@%serverip% "cat >> .ssh/authorized_keys"
 type $env:USERPROFILE\.ssh\id_rsa.pub | ssh root@<serverip> "cat >> .ssh/authorized_keys"
 
-rem Access the server without password
+rem # Access the server without password
 ssh root@%serverip% -o ServerAliveInterval=100
