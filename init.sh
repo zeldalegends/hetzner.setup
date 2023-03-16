177e5f2ab60ac37fccf98a760c295bbdc0da826b
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-16 17:47:03 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-# Script to rn as root to initializa a new Hetzner Cloud server
+# Run this script as root to initialize a new Hetzner Cloud server
 
 # Update repositories and system
-apt update && apt upgrade -y # apt-get update && apt-get upgrade -y 
+apt update && apt upgrade -y # apt-get update && apt-get upgrade -y
 
 # Enable SSH with private key
-echo Press Enter and add the private key ("ssh-rsa...") && read key
-nano .ssh/authorized_keys # add "ssh-rsa..."
-echo Press Enter and modify "PasswordAuthentication no" && read key
+echo 'Press Enter and add the private key ("ssh-rsa...")' && read key
+nano ~/.ssh/authorized_keys # add "ssh-rsa..."
+echo 'Press Enter and modify "PasswordAuthentication no"' && read key
 nano /etc/ssh/sshd_config # modify "PasswordAuthentication no"
 mv /etc/ssh/sshd_config.d/50-cloud-init.conf /etc/ssh/sshd_config.d/50-cloud-init.conf~
 service ssh restart # systemctl restart ssh.service
 
-# Fix locale settings 
+# Fix locale settings
 localectl set-locale C.UTF-8
 
 # (Optional) Add a normal user with sudo priviledges and SSH access
 adduser ubuntu
 adduser ubuntu sudo # usermod -aG sudo ubuntu
-echo Press Enter and add "ubuntu ALL=(ALL) NOPASSWD:ALL" && read key
+echo 'Press Enter and add "ubuntu ALL=(ALL) NOPASSWD:ALL"' && read key
 visudo -f /etc/sudoers.d/90-cloud-init-users # add "ubuntu ALL=(ALL) NOPASSWD:ALL"
+mkdir /home/ubuntu/.ssh
+cp /root/.ssh/authorized_keys /home/ubuntu/.ssh
+chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+
 su ubuntu
-mkdir .ssh
-sudo cp /root/.ssh/authorized_keys .ssh
 
 # Tested on Ubuntu 22.04.2 LTS
