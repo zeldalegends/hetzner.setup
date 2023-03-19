c156a767fd278d0bd66efc57b3495edd6e927e9f
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-19 05:52:40 +0100

    Update server.js

--- ubuntu-vm/server.js
+++ ubuntu-vm/server.js
@@ -2,20 +2,22 @@
 
 const express = require('express');
 
-// Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/:path', (req, res) => { // GET /test?a=b&c=1
+  console.log("Headers: " + JSON.stringify(req.headers, null, 3)); // {...}
+  console.log("Body: " + JSON.stringify(req.body, null, 3)); // {}
+  console.log("Path: " + req.params.path); // "test"
+  console.log("Query: " + JSON.stringify(req.query, null, 3)); // "test"
+  res.json(req.query); // {"a": "b", "c": "1"}
 });
 
-app.get('/:path', (req, res) => { // GET /test?a=b&c=1
-  console.log(req.params); // {"path": "test"}
-  res.json(req.query); // {"a": "b", "c": "1"}
+app.get('/*', (req, res) => {
+  res.send('<h2 style="color: blue">Usage: <code>http://' + req.headers.host + '/<i>path</i>?<i>query</i></code></h2>' +
+           '<h3>Example: <a href="/test?a=b&c=1"><code>http://' + req.headers.host + '/test?a=b&c=1</code></a></h3>');
 });
 
 const server = app.listen(PORT, HOST, () => {
