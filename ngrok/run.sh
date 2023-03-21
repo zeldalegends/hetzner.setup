f7eb25752267ad7b877e7e5944c31aa54ef23eef
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-21 13:45:55 +0100

    Update run.sh

--- ngrok/run.sh
+++ ngrok/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# docker run -it --rm --name ngrok --network private -e NGROK_AUTHTOKEN=2Mzg7mT5Xdbb4VCQ3VvtpE4kNrA_6W8eMDEddVRVEqZtrhF9i ngrok/ngrok:latest: http node-red:1880
+# docker run -it --rm --name ngrok --network private -e NGROK_AUTHTOKEN=2Mzg7mT5Xdbb4VCQ3VvtpE4kNrA_6W8eMDEddVRVEqZtrhF9i ngrok/ngrok:latest http node-red:1880
 docker run -it --rm --name ngrok --network private -v $(pwd)/ngrok.yml:/etc/ngrok.yml -e NGROK_CONFIG=/etc/ngrok.yml ngrok/ngrok:latest start --all
